f87b92ca1fd02a97d2e4652008b9204d8abf0720
Esseh <kennethwilleford@gmail.com>
2016-05-23 09:50:39 -0700

    [Plugin] A little added flexibility to the AJAX function.

--- Plugin/SunMoonNetworking.js
+++ Plugin/SunMoonNetworking.js
@@ -12,8 +12,9 @@
 	// Another object is taken in for a response. (Optional)
 	//	{
 	//		ret: Object Variable	If exists it will bind the response text to the variable under the field 'ret'
-	//		callback: function		If exists it will use the callback function with the response text as the parameter.
+	//		callback: function		If exists it will use the callback function.
 	//		parameters: [...]		Must exist alongside a callback. It will use the callback but applying each of the parameters in order.
+	//		use_response: true		Includes the response text as first parameter of the callback. 
 	//  }
 	// ===================================================================================
 	ajax = function(req,res){
@@ -30,8 +31,11 @@
 				if(res["ret"]        !== undefined){ res["ret"].ret = xhttp.responseText; }	
 				// If parameters weren't specified default to an empty array.
 				if(res["parameters"] === undefined){ res["parameters"] = []; }
+				if(res["use_response" !== undefined]){
+					res["parameters"] = [xhttp.responseText].concat(res["parameters"])
+				}
 				// If a callback was specified then execute it with optional parameters.
-				if(res["callback"]   !== undefined){ res["callback"].apply({},[xhttp.responseText].concat(res["parameters"])); }
+				if(res["callback"]   !== undefined){ res["callback"].apply({},res["parameters"]); }
 			}		
 		}
 	  }
